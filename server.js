0324433673d32cf7f790992c7a0d4494d41bcf41
hmada.m.k <hmada.kamel2006@gmail.com>
2021-12-07 05:35:44 +0200

    initial

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('puplic'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
@@ -21,5 +24,6 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.error('Connected to Mongoose'))
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
