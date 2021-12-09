0296ead03c57a378180aae66074d65528d6983d2
hmada.m.k <hmada.kamel2006@gmail.com>
2021-12-09 08:52:36 +0200

    asd

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
-app.use(express.static('puplic'))
+app.use('/static', express.static('public'))
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
